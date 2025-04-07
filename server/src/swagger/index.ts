18b07476b65fd0b03ab034c3b9a1fed41dcaf283
Yonas <se.yonas.assefa@gmail.com>
2025-04-07 18:07:19 +0300

    fix: swagger route

--- server/src/swagger/index.ts
+++ server/src/swagger/index.ts
@@ -1,14 +1,12 @@
 import swaggerJsdoc from "swagger-jsdoc";
 import path from "path";
 
-// Determine the environment (development or production)
 const isProduction = process.env.NODE_ENV === "production";
 
-// Set the path to the route files based on the environment
 const routeFiles = isProduction
-  ? path.join(__dirname, "../../dist/presentation/routes/*.js")
-  : path.join(__dirname, "../presentation/routes/*.ts"); 
-
+  ? path.join(__dirname, "../../dist/src/presentation/routes/*.js")
+  : path.join(__dirname, "../presentation/routes/*.ts");
+console.log("Route files path:", routeFiles);
 const options = {
   definition: {
     openapi: "3.0.0",
@@ -19,7 +17,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.BASE_URL || "http://localhost:3000",
+        url: process.env.BASE_URL || "http://localhost:3000", // Fallback to localhost if BASE_URL is not set
         description: "Development server",
       },
     ],
