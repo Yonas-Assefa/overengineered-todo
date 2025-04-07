5e21aeab52a29338de4de2c68a610f63e1c60d65
Yonas <se.yonas.assefa@gmail.com>
2025-04-07 17:52:18 +0300

    fix: swagger ui

--- server/src/swagger/index.ts
+++ server/src/swagger/index.ts
@@ -1,6 +1,14 @@
 import swaggerJsdoc from "swagger-jsdoc";
 import path from "path";
 
+// Determine the environment (development or production)
+const isProduction = process.env.NODE_ENV === "production";
+
+// Set the path to the route files based on the environment
+const routeFiles = isProduction
+  ? path.join(__dirname, "../../dist/presentation/routes/*.js")
+  : path.join(__dirname, "../presentation/routes/*.ts"); 
+
 const options = {
   definition: {
     openapi: "3.0.0",
@@ -11,12 +19,12 @@ const options = {
     },
     servers: [
       {
-        url: process.env.BASE_URL,
+        url: process.env.BASE_URL || "http://localhost:3000",
         description: "Development server",
       },
     ],
   },
-  apis: [path.join(__dirname, "../presentation/routes/*.ts")],
+  apis: [routeFiles],
 };
 
 export const swaggerSpec = swaggerJsdoc(options);
